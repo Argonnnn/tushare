64b6715fd2362877c5e97aeb2f1d6faa0c14bcee
Argonnnn <37509563+Argonnnn@users.noreply.github.com>
2021-11-08 23:12:52 +0800

    Add files via upload

--- sortdata.py
+++ sortdata.py
@@ -8,7 +8,7 @@ import tushare as ts
 import os
 pro = ts.pro_api()
 
-os.chdir('D:\data')
+os.chdir('E:\data')
 yearList = os.listdir()
 dfList = []
 for j in yearList:
@@ -25,14 +25,14 @@ for i in name:
     targetF.to_pickle(i+'.pkl')
 
 
-os.chdir(r'D:\factor\basic')
+os.chdir(r'E:\factor\basic')
 close = pd.read_pickle('close.pkl')
 close.index = [int(q) for q in close.index]
 startDay = max(close.index)
 endDay = dt.datetime.today().strftime('%Y%m%d')
 tradeD = pro.trade_cal(exchange='', start_date=startDay, end_date=endDay)
 tradeD = tradeD[tradeD.is_open==1]
-tradeList = tradeD.cal_date.to_list()
+tradeList = list(tradeD.cal_date)
 if len(close.loc[startDay,:].dropna()):
     tradeList.remove(str(startDay))
 
@@ -47,3 +47,19 @@ for i in name:
     toSave.index = [str(q) for q in toSave.index]
     toSave.to_pickle(i+'.pkl')
 
+##补充复权因子
+tradeD = pro.trade_cal(exchange='', start_date='20110101', end_date=endDay)
+tradeD = tradeD[tradeD.is_open==1]
+tradeList = list(tradeD.cal_date)
+
+
+def get_factor(day):
+    return pro.adj_factor(ts_code='', trade_date=day)
+import tqdm
+test = tradeD.cal_date.to_list()
+adjList = test.apply(get_factor,).to_list()
+ans = list(map(get_factor,test))
+df = pd.concat(ans)
+toSave = df.pivot(columns= 'ts_code',index = 'trade_date',values = 'adj_factor')
+toSave.index = [str(q) for q in toSave.index]
+toSave.to_pickle('adj_factor.pkl')
